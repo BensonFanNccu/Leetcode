1186856202f950f34a6136774491e449c2b77a9e
BensonFanNccu <110306044@g.nccu.edu.tw>
2024-06-08 22:59:07 +0800

    1. Two Sum(Add Annotations)

--- Java/Leetcode0001. Two Sum/src/Solution.java
+++ Java/Leetcode0001. Two Sum/src/Solution.java
@@ -2,11 +2,14 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class Solution {
+
+    //O(n) Solution
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 
         for(int i = 0; i < nums.length; i++){
             int complement = target - nums[i];
+
             if(map.containsKey(complement)){
                 return new int[] {map.get(complement), i};
             }
@@ -16,6 +19,7 @@ public class Solution {
         return new int[] {};
     }
 
+    //O(n^2) Solution
     public int[] twoSum2(int[] nums, int target) {
         for(int i = 0; i < nums.length; i++){
             for(int j = i + 1; j < nums.length; j++){
