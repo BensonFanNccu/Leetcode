432c33a6033e77a15f65b9d4fdbeb9acac2fcff4
BensonFanNccu <110306044@g.nccu.edu.tw>
2024-06-18 15:01:33 +0800

    7. Reverse Integer

---
+++ Java/Leetcode00007. Reverse Integer/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution sol = new Solution();
+
+        int case1 = -123;
+        int case2 = 120;
+
+        System.out.println(sol.reverse(case1) + " is the reverse of " + case1);
+        System.out.println(sol.reverse(case2) + " is the reverse of " + case2);
+    }
+}
