9a1d1fc53e20ef96202441fbca9944baa511fb22
BensonFanNccu <110306044@g.nccu.edu.tw>
2024-06-18 16:40:02 +0800

    231. Power of Two

---
+++ Java/Leetcode0231. Power of Two/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution sol = new Solution();
+
+        System.out.println(sol.isPowerOfTwo(1));
+        System.out.println(sol.isPowerOfTwo(4));
+        System.out.println(sol.isPowerOfTwo(7));
+    }
+}
