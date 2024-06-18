af3508903cb42e3f1025a56e6990901e0d10f703
BensonFanNccu <110306044@g.nccu.edu.tw>
2024-06-18 17:25:24 +0800

    709. To Lower Case

---
+++ Java/Leetcode0709. To Lower Case/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution sol = new Solution();
+
+        String case1 = "HeLLo WoRld";
+        String case2 = "lEeTcOdE";
+        String case3 = "lOwErCaSE";
+
+        System.out.println(sol.toLowerCase(case1));
+        System.out.println(sol.toLowerCase(case2));
+        System.out.println(sol.toLowerCase(case3));
+    }
+}
