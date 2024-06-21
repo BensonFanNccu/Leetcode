80d1445a5b0a676d7d0e02917e0c42e57b99a018
BensonFanNccu <110306044@g.nccu.edu.tw>
2024-06-21 21:31:00 +0800

    326. Power of Three

---
+++ Java/Leetcode0326. Power of Three/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution sol = new Solution();
+
+        System.out.println(sol.isPowerOfThree(81));
+        System.out.println(sol.isPowerOfThree(243));
+        System.out.println(sol.isPowerOfThree(56));
+    }
+}
