ed38a2f73f593c48d460182779e2f0ad00dca780
BensonFanNccu <110306044@g.nccu.edu.tw>
2024-06-21 10:06:01 +0800

    2769. Find the Maximum Achievable Number

---
+++ Java/Leetcode2769. Find the Maximum Achievable Number/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution sol = new Solution();
+
+        System.out.print("Maximum number after " + 1 + " operation of 4 is " + sol.theMaximumAchievableX(4, 1));
+        System.out.print("Maximum number after " + 2 + " operation of 3 is " + sol.theMaximumAchievableX(3, 2));
+    }
+}
