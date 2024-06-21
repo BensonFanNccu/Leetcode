187785ee284c05ba4289d01133086e5bfe67440f
BensonFanNccu <110306044@g.nccu.edu.tw>
2024-06-21 10:01:11 +0800

    709. To Lower Case(Without built-in)

--- Java/Leetcode0709. To Lower Case/src/Solution.java
+++ Java/Leetcode0709. To Lower Case/src/Solution.java
@@ -2,4 +2,17 @@ public class Solution {
     public String toLowerCase(String s) {
         return s.toLowerCase();
     }
+
+    //Without built-in
+    public String toLowerCase2(String s){
+        char[] arr = s.toCharArray();
+
+        for(int i = 0; i < arr.length; i++){
+            if ('A' <= arr[i] && arr[i] <= 'Z'){           
+                arr[i] = (char) (arr[i] - 'A' + 'a');
+            }
+        }
+
+        return new String(arr);
+    }
 }
