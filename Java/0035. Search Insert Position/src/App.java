0df7206daa3c77d9a83673543236bdade635d3a6
BensonFanNccu <110306044@g.nccu.edu.tw>
2025-02-20 20:35:17 +0800

    0035: Init commit

---
+++ Java/0035. Search Insert Position/src/App.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*
+         * Given a sorted array of distinct integers and a target value, 
+         * return the index if the target is found. 
+         * If not, return the index where it would be if it were inserted in order.
+         * 
+         * Example:
+         *     Input: nums = [1,3,5,6], target = 5
+         *     Output: 2
+         */
+
+        Solution sol = new Solution();
+
+        int[] testCase = new int[] {1,3,5,6};
+
+        System.out.println("nums: " + Arrays.toString(testCase) + " target: " + 5 + " Position: " + sol.searchInsert(testCase, 5));
+        System.out.println("nums: " + Arrays.toString(testCase) + " target: " + 2 + " Position: " + sol.searchInsert2(testCase, 2));
+        System.out.println("nums: " + Arrays.toString(testCase) + " target: " + 7 + " Position: " + sol.searchInsert(testCase, 7));
+    }
+}
