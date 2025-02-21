3028cd0f6788cc3e05869c3d1589f8d9b9cabe74
BensonFanNccu <110306044@g.nccu.edu.tw>
2025-02-21 14:31:10 +0800

    0083: Init commit

---
+++ Java/0083. Remove Duplicates from Sorted List/src/App.java
@@ -0,0 +1,28 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*
+         * Given the head of a sorted linked list, 
+         * delete all duplicates such that each element appears only once. 
+         * Return the linked list sorted as well.
+         * 
+         * Example:
+         *     Input: head = [1,1,2]
+         *     Output: [1,2]
+         */
+        
+        Solution sol = new Solution();
+
+        ListNode case1 = new ListNode(new int[] {1, 1, 2});
+        ListNode case2 = new ListNode(new int[] {1, 1, 2, 3, 3});
+
+        case1.printNodes();
+        System.out.print(" Output: ");
+        sol.deleteDuplicates(case1).printNodes();
+
+        System.out.println();
+
+        case2.printNodes();
+        System.out.print(" Output: ");
+        sol.deleteDuplicates(case2).printNodes();
+    }
+}
