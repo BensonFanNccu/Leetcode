ad2b31a7d8727e4d197adfa662d4daffb0ea0e94
BensonFanNccu <110306044@g.nccu.edu.tw>
2025-02-21 14:20:43 +0800

    0064: Correct output

--- Java/0064. Minimum Path Sum/src/App.java
+++ Java/0064. Minimum Path Sum/src/App.java
@@ -18,7 +18,7 @@ public class App {
         int[][] case1 = new int[][] {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
         int[][] case2 = new int[][] {{1, 2, 3}, {4, 5, 6}};
 
-        System.out.println("Input: " + Arrays.deepToString(case1) + " Ans: " + sol.minPathSum(case1) + ".");
-        System.out.println("Input: " + Arrays.deepToString(case2) + " Ans: " + sol.minPathSum(case2) + ".");
+        System.out.println("Input: " + Arrays.deepToString(case1) + " Ans: " + sol.minPathSum(case1));
+        System.out.println("Input: " + Arrays.deepToString(case2) + " Ans: " + sol.minPathSum(case2));
     }
 }
