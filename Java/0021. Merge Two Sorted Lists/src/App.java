5d5a4742947b14ee8c10a6cd175d4159402454ba
BensonFanNccu <110306044@g.nccu.edu.tw>
2025-02-21 15:29:01 +0800

    0021: Correct indent

--- Java/0021. Merge Two Sorted Lists/src/App.java
+++ Java/0021. Merge Two Sorted Lists/src/App.java
@@ -7,8 +7,8 @@ public class App {
          * Return the head of the merged linked list.
          * 
          * Example:
-         *     Input: list1 = [1,2,4], list2 = [1,3,4]
-         *     Output: [1,1,2,3,4,4]
+         * Input: list1 = [1,2,4], list2 = [1,3,4]
+         * Output: [1,1,2,3,4,4]
          */
         
         Solution sol = new Solution();
