fcf63e092f87df5957bc42036eab70fb902896a1
BensonFanNccu <110306044@g.nccu.edu.tw>
2025-02-22 14:14:26 +0800

    0231: Add example

--- Java/0231. Power of Two/src/App.java
+++ Java/0231. Power of Two/src/App.java
@@ -2,9 +2,12 @@ public class App {
     public static void main(String[] args) throws Exception {
         /*
          * Given an integer n, return true if it is a power of two. Otherwise, return false.
-         * An integer n is a power of two, if there exists an integer x such that n == 2x.
+         * An integer n is a power of two, if there exists an integer x such that n == 2^x.
          * 
-         * Example
+         * Example:
+         * Input: n = 16
+         * Output: true
+         * Explanation: 24 = 16
          */
 
         Solution sol = new Solution();
