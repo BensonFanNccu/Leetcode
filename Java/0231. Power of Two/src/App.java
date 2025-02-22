a87b8fdea0487cc84faf19a1a457fedda1eb6e67
BensonFanNccu <110306044@g.nccu.edu.tw>
2025-02-22 13:57:59 +0800

    0231: Init commit

---
+++ Java/0231. Power of Two/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*
+         * Given an integer n, return true if it is a power of two. Otherwise, return false.
+         * An integer n is a power of two, if there exists an integer x such that n == 2x.
+         * 
+         * Example
+         */
+
+        Solution sol = new Solution();
+
+        System.out.println(sol.isPowerOfTwo(1));
+        System.out.println(sol.isPowerOfTwo2(4));
+        System.out.println(sol.isPowerOfTwo(7));
+    }
+}
