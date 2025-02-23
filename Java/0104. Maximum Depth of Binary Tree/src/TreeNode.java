19fa69b9c37d21ffd47a60928f5a9df5068efbfe
BensonFanNccu <110306044@g.nccu.edu.tw>
2025-02-23 15:42:46 +0800

    0104: Correct indent

--- Java/0104. Maximum Depth of Binary Tree/src/TreeNode.java
+++ Java/0104. Maximum Depth of Binary Tree/src/TreeNode.java
@@ -2,13 +2,13 @@ public class TreeNode {
     int val;
     TreeNode left, right;
 
-    public TreeNode(int val){
+    public TreeNode(int val) {
         this.val = val;
         this.left = null;
         this.right = null;
     }
 
-    public TreeNode(int val, TreeNode left, TreeNode right){
+    public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
